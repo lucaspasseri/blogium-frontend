a34c4cce4301b357b3bcbb55106cc498858da8af
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-09 19:35:52 -0300

    Get posts done.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,16 @@ export default function HomePage() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    setPosts([{
-      id: 1,
-      title: 'Hello World',
-      coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-      contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-      content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-      commentCount: 2
-    }]);
+
+    const request= axios.get("http://localhost:4001");
+
+    request.then( response => {
+      console.log(response);
+      setPosts(response.data);
+    });
+
+    request.catch(error=>alert(error));
+
   }, []);
 
   return (
